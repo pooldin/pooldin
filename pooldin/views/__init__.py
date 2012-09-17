6155039bacf301db3325379033dc42ece81209bb
Brian Oldfield <brian.oldfield@gmail.com>
2012-09-16 20:35:13 -0400

    Initial user authentication  interface.

--- pooldin/views/__init__.py
+++ pooldin/views/__init__.py
@@ -1,10 +1,11 @@
 from .home import ViewHomeIndex
 from .campaign import ViewCampaignCreate, ViewCampaignDetails
-from .user import ViewUserLogin
+from .user import ViewUserLogin, ViewUserLogout
 
 all = [
     ViewHomeIndex,
     ViewUserLogin,
+    ViewUserLogout,
     ViewCampaignCreate,
     ViewCampaignDetails,
 ]
--- pooldin/views/user.py
+++ pooldin/views/user.py
@@ -1,8 +1,11 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, session, flash, url_for
+from flask.ext.login import login_user, logout_user
 
 from ..app.negotiate import supports
 from .base import BaseView
 from ..forms import FormUserLogin
+
+from ..database.models import User, Email
 
 
 class ViewUserLogin(BaseView):
@@ -14,6 +17,9 @@ class ViewUserLogin(BaseView):
         app.add_post_rule('/login', view_func=view)
 
     def get(self):
+        next = request.args.get('next')
+        if next:
+            session['next'] = next
         return render_template('user/login.html', form=FormUserLogin())
 
     @supports('application/json', 'application/x-www-form-urlencoded')
@@ -23,9 +29,49 @@ class ViewUserLogin(BaseView):
         else:
             data = request.json
 
-        form = FormUserLogin(data)
+        form = FormUserLogin(**data)
 
         valid = form.validate()
 
         if not valid:
             return render_template('user/login.html', form=form)
+
+        # Try and retrieve user by username first
+        user = User.query.filter_by(username=form.login.data).first()
+        if user is None:
+            email = Email.query.filter_by(address=form.login.data).first()
+            user = email.user.first() if email else None
+
+        if not user or not user.is_password(form.password.data) or not user.enabled:
+            flash('Unknown username/password combination.')
+            return redirect(url_for('user_login'))
+
+        login_user(user)
+        next = session.get('next', request.args.get('next'))
+        if 'next' in session:
+            del session['next']
+        return redirect(next or '/')
+
+
+class ViewUserLogout(BaseView):
+    name = 'user_logout'
+
+    @classmethod
+    def add_routes(cls, app, view):
+        app.add_get_rule('/logout', view_func=view)
+        app.add_post_rule('/logout', view_func=view)
+
+    def get(self):
+        logout_user()
+        return redirect('/')
+
+
+class ViewUserProfile(BaseView):
+    name = 'user_profile'
+
+    @classmethod
+    def add_routes(cls, app, view):
+        app.add_get_rule('/<string:user_id>', view_func=view)
+
+    def get(self, user_id):
+        return render_template('user/profile.html')
