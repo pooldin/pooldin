0b90251aabbc088d4539376a28cd83027c6e736b
Brian Oldfield <brian.oldfield@gmail.com>
2012-09-28 10:58:42 -0700

    Fixed bug in DateTimeTZ

--- pooldin/database/types/datetime.py
+++ pooldin/database/types/datetime.py
@@ -7,16 +7,23 @@ class DateTimeTZ(TypeDecorator):
     impl = DateTime
 
     def __init__(self, tzinfo=pytz.UTC, *args, **kwargs):
+        kwargs['timezone'] = True
         super(DateTimeTZ, self).__init__(*args, **kwargs)
         self.tz = tzinfo
 
     def process_bind_param(self, value, dialect):
+        if not value:
+            return value
         if value.tzinfo is None:
             value = self.tz.localize(value)
         elif value.tzname() != self.tz.zone:
-            value = value.astimezone(self.tmz)
+            value = value.astimezone(self.tz)
         return value
 
     def process_result_value(self, value, dialect):
-        value = self.tz.localize(value)
+        if value:
+            if value.tzinfo is None:
+                value = self.tz.localize(value)
+            elif value.tzname() != self.tz.zone:
+                value = value.astimezone(self.tz)
         return value
