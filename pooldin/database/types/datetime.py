1c58c237182a41476128a1bcb3b1188c99481936
Brian Oldfield <brian.oldfield@gmail.com>
2012-09-28 00:12:05 -0700

    Fixed a bug in tz aware DateTime db type.

--- pooldin/database/types/datetime.py
+++ pooldin/database/types/datetime.py
@@ -1,22 +1,22 @@
 import pytz
 
-from sqlalchemy.types import TypeDecorator, DATETIME
+from sqlalchemy.types import TypeDecorator, DateTime
 
 
 class DateTimeTZ(TypeDecorator):
-    impl = DATETIME
+    impl = DateTime
 
-    def __init__(self, timezone=pytz.UTC):
-        super(DateTimeTZ, self).__init__()
-        self.timezone = timezone
+    def __init__(self, tzinfo=pytz.UTC, *args, **kwargs):
+        super(DateTimeTZ, self).__init__(*args, **kwargs)
+        self.tz = tzinfo
 
     def process_bind_param(self, value, dialect):
         if value.tzinfo is None:
-            value = self.timezone.localize(value)
-        elif value.tzname() != self.timezone.zone:
-            value = value.astimezone(self.timezone)
+            value = self.tz.localize(value)
+        elif value.tzname() != self.tz.zone:
+            value = value.astimezone(self.tmz)
         return value
 
     def process_result_value(self, value, dialect):
-        value = self.timezone.localize(value)
+        value = self.tz.localize(value)
         return value
